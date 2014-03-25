1634784a98b4ecdac75cbf642a0a96d1029ba0cc
Sean Murray <smurray@ontraport.com>
2014-03-24 20:00:10 -0700

    api update

--- api/index.php
+++ api/index.php
@@ -1,27 +1,76 @@
 <?php
 	header("Access-Control-Allow-Origin: ". trim($_SERVER["HTTP_REFERER"], "/") );
 
-	if ($_POST["action"] == "join" && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
-	{
-		try {
-			
-			error_log("try this plz: " . $_POST["email"]);
-			$dbh = new PDO("mysql:host=localhost;dbname=satanbarbara", "satanbarbara", "iwilldie");
-			
-			$statement = $dbh->prepare("INSERT INTO `accounts` (`email`) VALUES (?)");
+	if (isset($_POST["action"])){
 		
-			$statement->execute(array(mysql_real_escape_string($_POST["email"])));
+		try {
+			$dbh = new PDO("mysql:host=localhost;dbname=satanbarbara", "satanbarbara", "iwilldie");
 		} catch (PDOException $e) {
 		    error_log('Connection failed: ' . $e->getMessage());
+			echo json_encode(array(
+				"data" => array(),
+				"status" => array(
+					"code" => 2,
+					"message" => "Connection failed"
+				)
+			));
 		}
+		
+		if ($_POST["action"] == "join" && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+			try {
+				$statement = $dbh->prepare("INSERT INTO `accounts` (`email`) VALUES (?)");
+		
+				$statement->execute(array(mysql_real_escape_string($_POST["email"])));
+			} catch (PDOException $e) {
+				echo json_encode(array(
+					"data" => array(),
+					"status" => array(
+						"code" => 2,
+						"message" => $e->getMessage()
+					)
+				));
+			}
+			
+			echo json_encode(array(
+				"data" => array(),
+				"status" => array(
+					"code" => 0,
+					"message" => "Success"
+				)
+			));
+		}
+		
+		if ($_POST["action"] == "getEmails"){
+			try {
+				$statement = $dbh->prepare("SELECT * FROM `accounts`");
+		
+				$statement->execute(array(mysql_real_escape_string($_POST["email"])));
+			} catch (PDOException $e) {
+				echo json_encode(array(
+					"data" => array(),
+					"status" => array(
+						"code" => 2,
+						"message" => $e->getMessage()
+					)
+				));
+			}
+			
+			echo json_encode(array(
+				"data" => $statement->fetchAll(PDO::FETCH_ASSOC),
+				"status" => array(
+					"code" => 0,
+					"message" => "Success"
+				)
+			));
+		}
+		
+	} else {
+		echo json_encode(array(
+			"status" => array(
+				"code" => 1,
+				"message" => "No action was provided"
+			)
+		));
 	}
 	
-	echo json_encode(array(
-		"data" => array(
-			
-		),
-		"status" => array(
-			"code" => 0,
-			"message" => "Success"
-		)
-	));
+
