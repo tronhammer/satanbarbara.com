1cfc4ab0ac10803d159c1ba4dab213b7e95e4ed3
pcm <pcm@pcm.loc>
2014-06-02 12:58:37 -0700

    adding resources

--- www/index.php
+++ www/index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<title>Satan Barbara</title>
-		<meta name="viewport" content="user-scalable=no,initial-scale = 1.0,maximum-scale = 1.0">
+		<meta name="viewport" content="user-scalable=no,initial-scale = 1.0,maximum-scale = 1.0" />
 		<!--[if lt IE 9]>
 		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+		<link href="/style/css/sb.global.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
-	<body style="padding:0px;margin:0px;height:900px;">
-		<div style="background: url(components/tile/tiles/events/images/bg.jpg) no-repeat 0 0; background-size: 100% 900px; width: 100%; height: 900px;">
-			<div style="background: url(components/tile/tiles/home/images/parchment.png) rgba(0, 0, 0, 0) no-repeat; background-size: 100% 600px; width: 50%; height: 600px; margin: 0 auto;">
-				<div style="padding: 115px 60px 115px 80px; max-height: 600px; overflow: auto;">
+	<body>
+		<div class="sb-main-container">
+			<div class="sb-signup-form">
+				<div class="sb-signup-form-content">
 					<h1>Welcome Tyrants!</h1>
 
 					<p>Here at SatanBarbara we are attempting to unite the metal scene in Santa Barbara. 
@@ -29,26 +31,7 @@
 			</div>
 		</div>
 		
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script>
-			$(function(){
-				$("body").delegate("#join", "click", function(){
-					$.ajax({
-						"url": "http://api.satanbarbara.com/",
-						"type": "post",
-						"data": {
-							"action": "join",
-							"email": $("#email").val()
-						}
-					}).done(function(){
-						$("#emailInputContainer").children().fadeOut();
-						$("#emailInputContainer").text("Thanks! We'll keep in touch");
-					}).fail(function(){
-						$("#emailInputContainer").children().fadeOut();
-						$("#emailInputContainer").text("There was an error! Try again later.");
-					});
-				});
-			});
-		</script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+		<script src="/js/sb.main.js" type="text/javascript"></script>
 	</body>
 </html>
